c77daee1f084709e534ad1acdbef5e60277ecd7c
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2019-10-04 13:17:31 +0100

    Update

--- tests/test_util.py
+++ tests/test_util.py
@@ -13,7 +13,7 @@ class UtilTestCase(unittest.TestCase):
         flattened_params = hamiltorch.util.flatten(model)
 
         new_model = nn.Linear(4, 4)
-        hamiltorch.util.unflatten(new_model, flattened_params)
+        new_model.params = hamiltorch.util.unflatten(new_model, flattened_params)
         new_model_flattened_params = hamiltorch.util.flatten(new_model)
 
         hamiltorch.util.eval_print('flattened_params', 'new_model_flattened_params')
