3e90360752d6531b7ec4fea5b5e64074fe356376
adcobb <acobb@robots.ox.ac.uk>
2019-10-04 21:37:08 +0100

    Get tests working

--- tests/test_util.py
+++ tests/test_util.py
@@ -13,7 +13,10 @@ class UtilTestCase(unittest.TestCase):
         flattened_params = hamiltorch.util.flatten(model)
 
         new_model = nn.Linear(4, 4)
-        new_model.params = hamiltorch.util.unflatten(new_model, flattened_params)
+        new_model_unflattened_params = hamiltorch.util.unflatten(new_model, flattened_params)
+        for weights, new_w in zip(model.parameters(), new_model_unflattened_params):
+            weights.data = new_w
+
         new_model_flattened_params = hamiltorch.util.flatten(new_model)
 
         hamiltorch.util.eval_print('flattened_params', 'new_model_flattened_params')
