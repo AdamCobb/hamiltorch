7ff428275b1c7e656ac8d30cc2bed7357c222320
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2019-10-10 14:17:11 +0100

    Functional tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -13,15 +13,52 @@ class UtilTestCase(unittest.TestCase):
         flattened_params = hamiltorch.util.flatten(model)
 
         new_model = nn.Linear(4, 4)
-        new_model_unflattened_params = hamiltorch.util.unflatten(new_model, flattened_params)
-        for weights, new_w in zip(new_model.parameters(), new_model_unflattened_params):
-            weights.data = new_w
+        unflattened_params = hamiltorch.util.unflatten(new_model, flattened_params)
+        hamiltorch.util.update_model_params_in_place(new_model, unflattened_params)
 
         new_model_flattened_params = hamiltorch.util.flatten(new_model)
 
         hamiltorch.util.eval_print('flattened_params', 'new_model_flattened_params')
 
         self.assertTrue(torch.all(torch.eq(flattened_params, new_model_flattened_params)))
+
+    def test_model_functional(self):
+        model = nn.Linear(4, 4)
+        fmodel = hamiltorch.util.make_functional(model)
+
+        x = torch.zeros(1, 4)
+        ymodel = model.forward(x)
+        params = list(model.parameters())
+        yfunctional = fmodel(x, params=params)
+
+        hamiltorch.util.eval_print('ymodel', 'yfunctional')
+
+        self.assertTrue(torch.all(torch.eq(ymodel, yfunctional)))
+
+    def test_differentiable_model_functional(self):
+        model = nn.Linear(2, 2)
+        params = list(model.parameters())
+        params_flattened = hamiltorch.util.flatten(model)
+        fmodel = hamiltorch.util.make_functional(model)
+
+        model.zero_grad()
+        xmodel = torch.randn(1, 2, requires_grad=True)
+        ymodel = model.forward(xmodel).sum()
+        ymodel.backward()
+        xmodelgrad = xmodel.grad
+        pmodelgrad = torch.cat([p.grad.flatten() for p in params])
+
+        model.zero_grad()
+        xfunctional = xmodel.detach().clone().requires_grad_()
+        yfunctional = fmodel(xfunctional, params=params).sum()
+        yfunctional.backward()
+        xfunctionalgrad = xfunctional.grad
+        pfunctionalgrad = torch.cat([p.grad.flatten() for p in params])
+
+        hamiltorch.util.eval_print('params_flattened', 'xmodel', 'ymodel', 'xmodelgrad', 'xfunctional', 'yfunctional', 'xfunctionalgrad', 'pmodelgrad', 'pfunctionalgrad')
+
+        self.assertTrue(torch.all(torch.eq(xmodelgrad, xfunctionalgrad)))
+        self.assertTrue(torch.all(torch.eq(ymodel, yfunctional)))
 
 
 if __name__ == '__main__':
