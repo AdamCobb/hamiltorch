5e304df9463d1bac58d48f21cb4fead844ca7842
adcobb <acobb@robots.ox.ac.uk>
2020-10-13 21:34:22 -0400

    Update to version 0.4.0.dev1

--- hamiltorch/__init__.py
+++ hamiltorch/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.1.dev3'
+__version__ = '0.4.0.dev1'
 
-from .samplers import sample, sample_model, predict_model, Sampler, Integrator, Metric
+from .samplers import sample, sample_model, predict_model, sample_split_model, Sampler, Integrator, Metric
 from .util import set_random_seed
