66a6aa769b317b370a827d34287d9a74afd6e9d5
abhijit brahme <brahmeabhijit@gmail.com>
2024-02-20 18:16:57 -0800

    added surrogates

--- hamiltorch/models.py
+++ hamiltorch/models.py
@@ -1,7 +1,7 @@
 import torch 
 import torch.nn as nn
-
-
+from torch.autograd import grad
+from torchdyn.core import NeuralODE
 
 class NNgHMC(nn.Module):
     """
@@ -61,15 +61,40 @@ class NNEnergy(nn.Module):
             )))
     
     
+class HNN(nn.Module):
+    def __init__(self, Hamiltonian: nn.Module) -> None:
+        super(HNN).__init__()
+        self.H = Hamiltonian
+    def forward(self, t, x):
+        with torch.set_grad_enabled(True):
+            n = x.shape[1] // 2
+            x = x.requires_grad_(True)
+            gradH = grad(self.H(x).sum(), x, create_graph=True)[0]
+        return torch.cat([gradH[:, n:], -gradH[:, :n]], 1).to(x)
+
+class HNNODE(nn.Module):
+    def __init__(self, odefunc: HNN, sensitivity="adjoint", solver="alf", atol=1e-3, rtol=1e-3) -> None:
+        super(HNNODE).__init__()
+        self.odefunc = odefunc
+        self.neural_ode_layer = NeuralODE(self.odefunc, solver = solver, sensitivity=sensitivity, atol=atol, rtol=rtol)
+    def forward(self, t, x, *args):
+        return self.neural_ode_layer.forward(x, t)
+
+
+
+
 
 def train(model: nn.Module, X, y, epochs = 10, lr = .01, loss = "l2"):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    print("Training Surrogate Model")
     for _ in range(epochs):
         y_pred = model(X)
 
         # Compute and print loss.
         if loss == "l2":
             loss = nn.MSELoss()(y_pred, y)
+        else:
+            raise ValueError
         # Before the backward pass, use the optimizer object to zero all of the
         # gradients for the variables it will update (which are the learnable
         # weights of the model). This is because by default, gradients are
